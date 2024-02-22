237f6bd801f3c5a80be84a6e37094abd002928e7
Dev Namdev <devnamdevcse@gmail.com>
2024-02-22 23:07:41 +0530

    update

--- event_management_system/urls.py
+++ event_management_system/urls.py
@@ -10,21 +10,7 @@ urlpatterns = [
     path('categories/<int:category_id>/', views.category_events, name='category_events'),
     path('categories/delete/<int:category_id>/', views.delete_category, name='delete_category'),
     
-    # URL patterns for event-related views
-    # path('events/', views.event_list, name='event_list'),
     path('events/create/', views.create_event, name='create_event'),
     path('events/update/<int:event_id>/', views.update_event, name='update_event'),
     path('events/delete/<int:event_id>/', views.delete_event, name='delete_event'),
-    
-    
-    # path('events/', views.event_list, name='event_list'),
-    # path('create/', views.create_event, name='create_event'),
-    # path('events/delete/<int:event_id>/',
-    #      views.delete_event, name='delete_event'),
-    # path('categories/', views.category_list, name='category_list'),
-    # path('create_category/', views.create_category, name='create_category'),
-    # path('delete_category/<int:category_id>/',
-    #      views.delete_category, name='delete_category'),
-    # path('category/<int:category_id>/',
-    #      views.category_events, name='category_events'),
 ]
--- event_management_system_app/admin.py
+++ event_management_system_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Category, Event
 
-# Register your models here.
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
